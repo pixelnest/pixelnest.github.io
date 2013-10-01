7c66d760d9f2717c26b5bc01dd4f5679d6fc5a08
Matthieu Oger <moger@pixelnest.io>
2013-10-01 16:32:51 +0200

    [TO REMOVE WHEN FIXED] Poly hack for WinChrome #3
    * Use serif with Chrome On Windows…
    * Using a SVG variant worked but it added a nasty bug making the text
    unreadable.
    
    So, here it is serif. Your turn. Hope you'll shine…

--- static/js/hackchrome.js
+++ static/js/hackchrome.js
@@ -4,16 +4,13 @@
 
 /*
  * Chrome on Windows is doing shit with some Google Webfonts.
- * In order to render the font properly, we use a local one in SVG.
- * Then, in JavaScript, we change the family to use this one in Chrome Windows.
+ * We tried to add a SVG variant of the custom font for Chrome, which worked.
+ * However, it adds a nasty bug that breaks the layout, making the text unreadable.
+ * We'd rather have a basic and without flavour font that an unreadable one.
  */
 
-// It is ugly, it is bad. But Chrome on Windows forces me to do that...
-// And I don't want to make the other browsers use the (automatically) converted font.
+// It is ugly, it is bad. But Chrome on Windows forces us to do that...
+// Remove the custom font and use serif instead. To remove when Chrome will be fixed.
 if (navigator.platform === 'Win32' && navigator.userAgent.indexOf('Chrome/') !== -1) {
-  // Change font
-  document.body.style.fontFamily = 'Chropoly, serif';
-
-  // Remove the bug in the quicknavbar
-  document.getElementById('quicknavbar').style.fontFamily = 'serif';
+  document.body.style.fontFamily = 'serif';
 }
