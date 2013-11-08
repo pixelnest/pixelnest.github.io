d5adb2101f9b58dc5b20a4d5f95d6ea1bdb692b3
Matthieu Oger <moger@pixelnest.io>
2013-11-08 16:26:01 +0100

    Add check

--- static/js/menu.js
+++ static/js/menu.js
@@ -2,9 +2,11 @@ var menu = document.getElementById('quicknavbar');
 
 // On change, get the current item and if the value
 // contains a path, use it to set a new location.
-menu.onchange = function () {
-  var value = this.item(this.selectedIndex).value;
-  if (value && -1 !== value.indexOf('/')) {
-    window.location = value;
-  }
-};
+if (menu) {
+  menu.onchange = function () {
+    var value = this.item(this.selectedIndex).value;
+    if (value && -1 !== value.indexOf('/')) {
+      window.location = value;
+    }
+  };
+}
